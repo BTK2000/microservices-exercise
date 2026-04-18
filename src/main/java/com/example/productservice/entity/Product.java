c7315756ce16be0e23d91ef345d98aab4df239f3
BTK2000 <bashyamtk@gmail.com>
2026-04-18 16:42:17 -0400

    1A - Product Entity

---
+++ src/main/java/com/example/productservice/entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
