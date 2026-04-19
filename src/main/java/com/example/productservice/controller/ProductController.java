28ab5304262aa455481ff3a1f1f337812f97d125
BTK2000 <bashyamtk@gmail.com>
2026-04-18 20:55:19 -0400

    1F - Product-pagination + sorting + streams

--- src/main/java/com/example/productservice/controller/ProductController.java
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.example.productservice.controller;
 
 import com.example.productservice.entity.Product;
 import com.example.productservice.service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -40,5 +42,19 @@ public class ProductController {
     public String deleteProduct(@PathVariable Integer id) {
         productService.deleteProduct(id);
         return "Product deleted successfully with id: " + id;
+    }
+
+    @GetMapping("/paged")
+    public Page<Product> getProductsPaged(
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam String sortBy
+    ) {
+        return productService.getProductsPaged(page, size, sortBy);
+    }
+
+    @GetMapping("/filter/price")
+    public List<Product> getProductsAbovePrice(@RequestParam BigDecimal price) {
+        return productService.getProductsAbovePrice(price);
     }
 }
